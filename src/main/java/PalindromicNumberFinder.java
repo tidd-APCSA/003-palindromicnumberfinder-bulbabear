699fd3bc2900fa33a1d3fa6c86ff58879a40f550
Jana Liu <jana.liu.07@gmail.com>
2020-11-09 15:25:57 +0000

    i wrote the code

--- src/main/java/PalindromicNumberFinder.java
+++ src/main/java/PalindromicNumberFinder.java
@@ -13,21 +13,39 @@ public class PalindromicNumberFinder {
 
     // this method should find the next palindromic number
     public int searchForPalindromicNum(int num){
+      int palo = 0;
+      for (int i = num +1; i > num; i++){
+        if (testPalindromicNum(i) == true)
+        {
+          palo = i;
+          break;
+        }
+        else
+        {
+          palo = 1;
+        }
+      }
+      return palo;
+  }
 
-    }
-
-    // this is a helper method for searchForPalindromicNum. It's purpose is to test if a number is actually a palindrome
+    // this is a helper method for searchForPalindromicNum. It's purpose is to test if a number is actually a palindrome. 72928
     public boolean testPalindromicNum(int num){
-
-
+      String test = String.valueOf(num);
+      if(reverseNum(num).equals(test)){
+        return true;
+      }else{
+        return false;
+      }
     }
-
 
     // this is a helper method for testPalindromicNum. It should reverse the number and return it.
     public String reverseNum(int num){
-
-
-
+      String test = String.valueOf(num);
+      String r1 = "";
+      for(int i= test.length()-1; i>=0; i--){
+          r1 = r1 + test.charAt(i);
+      }
+      return r1;
     }
 
 
